4eb446b9d63e35d26841073651e83e2f13edce2b
mjmoran123 <matthew.j.moran@gmail.com>
2016-10-14 17:29:44 -0500

    add cost function and gradient descent for multiple features

--- machine-learning-ex1/ex1/featureNormalize.m
+++ machine-learning-ex1/ex1/featureNormalize.m
@@ -9,7 +9,12 @@ function [X_norm, mu, sigma] = featureNormalize(X)
 X_norm = X;
 mu = zeros(1, size(X, 2));
 sigma = zeros(1, size(X, 2));
-
+for i = 1:columns(X)
+	mu(i) = mean(X(:,i));
+	sigma(i) = std(X(:,i));
+	X_norm(:,i) = X(:,i) .- mu(i);
+	X_norm(:,i) = X(:,i) ./ sigma(i);
+end
 % ====================== YOUR CODE HERE ======================
 % Instructions: First, for each feature dimension, compute the mean
 %               of the feature and subtract it from the dataset,
@@ -24,7 +29,9 @@ sigma = zeros(1, size(X, 2));
 %               each feature. 
 %
 % Hint: You might find the 'mean' and 'std' functions useful.
-%       
+%      
+
+
 
 
 
