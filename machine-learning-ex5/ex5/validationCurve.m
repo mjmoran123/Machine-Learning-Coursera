78bed66edf1f1146cb4c78af85b38c5970556c16
mjmoran123 <matthew.j.moran@gmail.com>
2016-11-10 15:41:37 -0600

    implement validation curve to choose lambda

--- machine-learning-ex5/ex5/validationCurve.m
+++ machine-learning-ex5/ex5/validationCurve.m
@@ -10,7 +10,7 @@ function [lambda_vec, error_train, error_val] = ...
 %
 
 % Selected values of lambda (you should not change this)
-lambda_vec = [0 0.001 0.003 0.01 0.03 0.1 0.3 1 3 10]';
+lambda_vec = transpose([0 0.001 0.003 0.01 0.03 0.1 0.3 1 3 10]);
 
 % You need to return these variables correctly.
 error_train = zeros(length(lambda_vec), 1);
@@ -39,7 +39,14 @@ error_val = zeros(length(lambda_vec), 1);
 %
 %
 
-
+for i = 1:length(lambda_vec)
+	lambda = lambda_vec(i);
+	t = trainLinearReg(X, y, lambda);
+	[J, grad] = linearRegCostFunction(X, y, t, 0);
+	error_train(i) = J;
+	[j, grad] = linearRegCostFunction(Xval, yval, t, 0);
+	error_val(i) = j;
+endfor
 
 
 
